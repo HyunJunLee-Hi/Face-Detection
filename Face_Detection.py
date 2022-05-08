84fc523d7c47b84c71328c9feec2e1edb1fa63c2
HyunJunLee-Hi <nejukja@gmail.com>
2022-05-08 18:11:30 +0900

    Face Detection + Gender and Age Prediction

--- Face_Detection.py
+++ Face_Detection.py
@@ -1,11 +1,14 @@
 import cv2
 import timeit
 
-def videoDetector(cam, cascade):
+def videoDetector(cam, cascade, age_net, gender_net, MODEL_MEAN_VALUES, age_list, gender_list):
     while True:
-        start_t = timeit.default_timer()
+        # start_t = timeit.default_timer()
         ret, img = cam.read()
-        img = cv2.resize(img, dsize=None, fx=0.75, fy=0.75)
+        try:
+            img = cv2.resize(img, dsize=None, fx=1.0, fy=1.0)
+        except:
+            break
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         results = cascade.detectMultiScale(gray,
                                            scaleFactor=1.1,
@@ -13,11 +16,25 @@ def videoDetector(cam, cascade):
                                            minSize=(20, 20))
         for box in results:
             x, y, w, h = box
-            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 255), thickness=2)
+            face = img[int(y):int(y+h), int(x):int(x+h)].copy()
+            blob = cv2.dnn.blobFromImage(face, 1, (227, 227), MODEL_MEAN_VALUES, swapRB=False)
 
-        terminate_t = timeit.default_timer()
-        FPS = 'fps' + str(int(1./(terminate_t - start_t)))
-        cv2.putText(img, FPS, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
+            gender_net.setInput(blob)
+            gender_preds = gender_net.forward()
+            gender = gender_preds.argmax()
+
+            age_net.setInput(blob)
+            age_preds = age_net.forward()
+            age = age_preds.argmax()
+
+            info = gender_list[gender] + ' ' + age_list[age]
+
+            cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 255), thickness=2)
+            cv2.putText(img, info, (x, y-15), 0, 0.5, (0, 255, 0), 1)
+
+        # terminate_t = timeit.default_timer()
+        # FPS = 'fps' + str(int(1./(terminate_t - start_t)))
+        # cv2.putText(img, FPS, (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1)
 
         cv2.imshow('Face_Detection', img)
 
@@ -28,11 +45,23 @@ def videoDetector(cam, cascade):
 cascade_filename = 'haarcascade_frontalface_alt.xml'
 cascade = cv2.CascadeClassifier(cascade_filename)
 
+MODEL_MEAN_VALUES = (78.4263377603, 87.7689143744, 114.895847746)
+
+age_net = cv2.dnn.readNetFromCaffe('deploy_age.prototxt',
+                                   'age_net.caffemodel')
+print("age : ", end='')
+print(age_net)
+gender_net = cv2.dnn.readNetFromCaffe('deploy_gender.prototxt',
+                                      'gender_net.caffemodel')
+print("gender : ", end='')
+print(gender_net)
+age_list = ['(0 ~ 2)','(4 ~ 6)','(8 ~ 12)','(15 ~ 20)',
+            '(25 ~ 32)','(38 ~ 43)','(48 ~ 53)','(60 ~ 100)']
+gender_list = ['Male', 'Female']
+
 cam = cv2.VideoCapture('sample.mp4')
-img = cv2.imread('sample.jpg')
 
-videoDetector(cam, cascade)
-
+videoDetector(cam,cascade,age_net,gender_net,MODEL_MEAN_VALUES,age_list,gender_list )
 
 
 # Reference
